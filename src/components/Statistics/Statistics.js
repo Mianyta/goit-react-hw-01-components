0ceeb7e6805e89d28087d68e686905c73538d597
Mianyta <suta180987@gmail.com>
2023-10-08 07:05:21 +0300

    Statistics

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './Statistics.module.css';
+
+export const Statistics = ({ stats, title }) => {
+  return (
+    <section className={styles.statistics}>
+      {title && <h2 className={styles.title}>{title}</h2>}
+      <ul className={styles.statList}>
+        {stats.map(item => (
+          <li className={styles.item} key={item.id}>
+            <span className={styles.label}>{item.label} </span>
+            <span className={styles.percentage}>{item.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
